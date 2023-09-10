a93f76dd6612f447fbea1bb4270cbe22f521ee55
MaksimDmitrijev <134190245+MaksimDmitrijev@users.noreply.github.com>
2023-09-10 10:20:52 +0300

    Add files via upload

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example.todolistappjavaee27 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.todolistappjavaee27 to javafx.fxml;
+    exports com.example.todolistappjavaee27;
+}
